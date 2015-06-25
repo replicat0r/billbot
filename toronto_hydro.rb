d103ea9882bf88060483ae72cf008a0af1e4ead5
Abdallah <abdallaharar@turnkii.com>
2015-06-25 19:11:38 -0400

    mechanise logs in to toronto hydo

---
+++ toronto_hydro.rb
@@ -0,0 +1,21 @@
+require 'rubygems'
+require 'mechanize'
+require 'nokogiri'
+
+login_cred = {
+	'user' => "",
+	'pass' => ""
+}
+login_url = "https://css.torontohydro.com/selfserve/Pages/login.aspx"
+login_field_names = {
+	'user' => 'ctl00$SPWebPartManager1$g_70b63f08_8d15_4c19_8991_940d987b2a56$ctl00$membershipLogin$UserName',
+	'pass' => 'ctl00$SPWebPartManager1$g_70b63f08_8d15_4c19_8991_940d987b2a56$ctl00$membershipLogin$Password'
+}
+
+mech = Mechanize.new
+mech.get(login_url)
+form = mech.page.form_with(:action=>/login.aspx/) 
+form[login_field_names['user']] = login_cred['user']
+form[login_field_names['pass']] = login_cred['pass']
+form.submit(form.button_with(:value=>'Login'))
+puts mech.page.parser.css("title").text.strip 
