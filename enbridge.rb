b6e8a0a9537447c4f080d9b6b7832b9e570f7f06
Abdallah <abdallaharar@turnkii.com>
2015-06-30 13:45:15 -0400

    scrapes bill history from embridge into csv format

--- enbridge.rb
+++ enbridge.rb
@@ -25,10 +25,15 @@ puts browser.title
 puts "Balance: " + browser.tds(:class => 'summaryinfo')[1].text 
 puts "Due Date: " + browser.tds(:class => 'summaryinfo')[0].text
 
-# browser.a(:text => 'Payments').click
-# browser.a(:text => 'Payment History').click
-# browser.a(:text => 'SUBMIT').click
-# browser.span(:text => 'Export to CSV').click
+browser.a(:text => 'Payments').click
+browser.a(:text => 'Payment History').click
+browser.a(:text => 'SUBMIT').click
+Watir::Wait.until {browser.iframe.table(:id => "rgrdPaymentHistory_ctl00").exists?}
+csv_data = "Amount,Date\n"
+browser.iframe.table(:id => "rgrdPaymentHistory_ctl00").trs.each{|x|
+    csv_data = csv_data + x.tds[2].text + "," + x.tds[1].text + "\n"
+}
+puts csv_data
 browser.close
 headless.destroy
 
